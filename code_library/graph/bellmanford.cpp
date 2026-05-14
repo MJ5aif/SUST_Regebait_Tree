47a275f08d9df93a49497ff912e5da77d516b384
NAHDI51 <hossainnahdi8051@gmail.com>
2026-05-15 00:30:58 +0600

    Removed macros

--- code_library/graph/bellmanford.cpp
+++ code_library/graph/bellmanford.cpp
@@ -1,11 +1,11 @@
 const ll inf = LLONG_MAX;
-struct Ed { int a, b, w, s() { return a < b ? a : -a; }};
+struct Ed { int a, b, w; ll s() { return a < b ? a : -a; }};
 struct Node { ll dist = inf; int prev = -1; };
 void bellmanFord(vector<Node>& nodes, vector<Ed>& eds, int s) {
 	nodes[s].dist = 0;
 	sort(all(eds), [](Ed a, Ed b) { return a.s() < b.s(); });
-	int lim = sz(nodes) / 2 + 2; // /3+100 with shuffled vertices
-	rep(i,0,lim) for (Ed ed : eds) {
+	int lim = nodes.size() / 2 + 2; // /3+100 with shuffled vertices
+	for(int i = 0; i < lim; i++) for (Ed ed : eds) {
 		Node cur = nodes[ed.a], &dest = nodes[ed.b];
 		if (abs(cur.dist) == inf) continue;
 		ll d = cur.dist + ed.w;
@@ -14,7 +14,7 @@ void bellmanFord(vector<Node>& nodes, vector<Ed>& eds, int s) {
 			dest.dist = (i < lim-1 ? d : -inf);
 		}
 	}
-	rep(i,0,lim) for (Ed e : eds) {
+	for(int i = 0; i < lim; i++) for (Ed e : eds) {
 		if (nodes[e.a].dist == -inf)
 			nodes[e.b].dist = -inf;
 	}
