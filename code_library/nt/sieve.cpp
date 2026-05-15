d09695a50e2fb92e3e55a4dba4bf6d0a3ac828fa
NAHDI51 <hossainnahdi8051@gmail.com>
2026-05-16 00:06:22 +0600

    Added SPF and totient

--- code_library/nt/sieve.cpp
+++ code_library/nt/sieve.cpp
@@ -1,12 +1,20 @@
-VI primes;
+vector<int> primes;
 bool isprime[MAXN];
+int spf[MAXN], phi[MAXN];
 void sieve() {
-  isprime[1] = false;
-  for (int i = 2; i < MAXN; ++i) isprime[i] = true;
-  for (int i = 2; i < MAXN; ++i) {
-    if (!isprime[i]) continue;
-    primes.pb(i);
-    if (1LL * i * i >= MAXN) continue;
-    for (int j = i * i; j < MAXN; j += i) isprime[j] = false;
+  for (int i = 0; i < MAXN; ++i) {
+      isprime[i] = true; spf[i] = i; phi[i] = i;
+  }
+  isprime[0] = isprime[1] = false;
+  
+  for (ll i = 2; i < MAXN; ++i) {
+      if (isprime[i]) {
+          primes.push_back(i); phi[i] = i - 1; 
+          for (ll j = 2 * i; j < MAXN; j += i) {
+              isprime[j] = false;
+              if (spf[j] == j) spf[j] = i;
+              phi[j] -= phi[j] / i;
+          }
+      }
   }
 }
