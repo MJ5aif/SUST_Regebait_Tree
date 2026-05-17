0db73801b9a0c4e01ded035f2cfa6c0906145a74
MysteriousBits <si.islam214@gmail.com>
2026-05-18 00:50:04 +0600

    Updated segtree template

--- code_library/ds/segtree.cpp
+++ code_library/ds/segtree.cpp
@@ -1,89 +1,38 @@
-
-template <typename T>
-struct segtree {
-#define left(u) 2 * u + 1
-#define right(u) 2 * u + 2
-  int n;
-  T init_val;
-  vector<T> tree;
-  function<T(T, T)> f;
-  bool is_lazy = false;
-  T init_lazy;
-  vector<T> lazy;
-  function<void(T&, T, int, int)> apply_lazy;
-  function<void(T&, T)> merge_lazy;
-  segtree() {}  // Dummy constructor for global declaration
-  segtree(int sz, function<T(T, T)> f, T val = 0) {
-    n = sz;
-    this->f = f;
-    init_val = val;
-    tree.assign(1 << (__lg(n - 1) + 2), init_val);
-  }
-  // apply(tree[u], lazy[u], l, r)
-  // merge(lazy[left(u)], lazy[u])
-  void make_lazy(function<void(T&, T, int, int)> apply,
-                 function<void(T&, T)> merge, T init = 0) {
-    is_lazy = true;
-    init_lazy = init;
-    apply_lazy = apply;
-    merge_lazy = merge;
-    lazy.assign(tree.size(), init_lazy);
-  }
-  void build(vector<T>& a) { _build(0, 0, n - 1, a); }
-  void update(int i, T x) { _update(0, 0, n - 1, i, x); }
-  void update(int l, int r, T x) { _update_range(0, 0, n - 1, l, r, x); }
-  T get(int l, int r) { return _get(0, 0, n - 1, l, r); }
-  void _build(int u, int l, int r, vector<T>& a) {
-    if (l == r) {
-      tree[u] = a[l];
+template <typename T>  // 1 based
+struct ST {
+#define lc (nd << 1)
+#define rc ((nd << 1) | 1)
+  vector<T> t;
+  T identity = 0;  // 0 for sum, 1e18 for min etc;
+  ST(int n) { t.resize(4 * n, identity); }
+  inline T combine(T a, T b) { return a + b; }
+  inline void pull(int nd) { t[nd] = combine(t[lc], t[rc]); }
+  void build(int nd, int b, int e, vector<T>& a) {
+    if (b == e) {
+      t[nd] = a[b];
       return;
     }
-    int mid = (l + r) / 2;
-    _build(left(u), l, mid, a);
-    _build(right(u), mid + 1, r, a);
-    tree[u] = f(tree[left(u)], tree[right(u)]);
+    int mid = (b + e) >> 1;
+    build(lc, b, mid);
+    build(rc, mid + 1, e);
+    pull(nd);
   }
-  void _update(int u, int l, int r, int i, T x) {
-    if (l == r) {
-      tree[u] = x;
+  void upd(int nd, int b, int e, int i, T v) {
+    if (b == e) {
+      t[nd] = v;  // For "add v", use t[nd] += v;
       return;
     }
-    int mid = (l + r) / 2;
+    int mid = (b + e) >> 1;
     if (i <= mid)
-      _update(left(u), l, mid, i, x);
+      upd(lc, b, mid, i, v);
     else
-      _update(right(u), mid + 1, r, i, x);
-    tree[u] = f(tree[left(u)], tree[right(u)]);
+      upd(rc, mid + 1, e, i, v);
+    pull(nd);
   }
-  void propagate(int u, int l, int r) {
-    if (lazy[u] == init_lazy || l == r) return;
-    int m = (l + r) / 2;
-    apply_lazy(tree[left(u)], lazy[u], l, m);
-    merge_lazy(lazy[left(u)], lazy[u]);
-    apply_lazy(tree[right(u)], lazy[u], m + 1, r);
-    merge_lazy(lazy[right(u)], lazy[u]);
-    lazy[u] = init_lazy;
+  T query(int nd, int b, int e, int i, int j) {
+    if (i > e || b > j) return identity;
+    if (i <= b && e <= j) return t[nd];
+    int mid = (b + e) >> 1;
+    return combine(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
   }
-  void _update_range(int u, int tl, int tr, int l, int r, T x) {
-    if (l > tr || r < tl) return;
-    if (tl >= l && tr <= r) {
-      apply_lazy(tree[u], x, tl, tr);
-      merge_lazy(lazy[u], x);
-      return;
-    }
-    propagate(u, tl, tr);
-    int mid = (tl + tr) / 2;
-    _update_range(left(u), tl, mid, l, r, x);
-    _update_range(right(u), mid + 1, tr, l, r, x);
-    tree[u] = f(tree[left(u)], tree[right(u)]);
-  }
-  T _get(int u, int tl, int tr, int l, int r) {
-    if (l > tr || r < tl) return init_val;
-    if (l <= tl && r >= tr) return tree[u];
-    if (is_lazy) propagate(u, tl, tr);
-    int mid = (tl + tr) / 2;
-    return f(_get(left(u), tl, mid, l, r), _get(right(u), mid + 1, tr, l, r));
-  }
-#undef left
-#undef right
 };
